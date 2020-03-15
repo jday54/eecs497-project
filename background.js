884a80da26f954c26068fe8cb4dac40bb9843ade
Austin Wong <auswong@umich.edu>
2020-03-15 17:47:33 -0400

    popup stuff

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//     chrome.storage.sync.set({color: '#3aa757'}, function() {
-//       console.log("The color is green.");
-//     });
-// });
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#FF7373'}, function() {
+      console.log("The color is blue.");
+    });
+});
 
 'use strict';
 chrome.runtime.onInstalled.addListener(function() {
@@ -13,14 +13,14 @@ chrome.runtime.onInstalled.addListener(function() {
     });
 });
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-});
+// chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//     chrome.declarativeContent.onPageChanged.addRules([{
+//         conditions: [new chrome.declarativeContent.PageStateMatcher({
+//             pageUrl: {hostEquals: 'developer.chrome.com'},
+//         })
+//         ],
+//             actions: [new chrome.declarativeContent.ShowPageAction()]
+//     }]);
+// });
 
-  chrome.browserAction.onClicked(object details, function callback);
+//   chrome.browserAction.onClicked(object details, function callback);
