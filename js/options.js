9e81c67cc4758cc21c27bdf1374f1165f4483d25
Jeff <jfguan@umich.edu>
2020-03-23 16:17:13 -0400

    small push

--- js/options.js
+++ js/options.js
@@ -41,24 +41,25 @@ chrome.storage.sync.get(null, function(items) {
 
 function addFields(){
   // Number of inputs to create
-  var number = document.getElementById("member").value;
+  var number = Object.keys(items).length;
   // Container <div> where dynamic content will be placed
   var container = document.getElementById("container");
   // Clear previous contents of the container
-  while (container.hasChildNodes()) {
-      container.removeChild(container.lastChild);
-  }
+  // while (container.hasChildNodes()) {
+  //     container.removeChild(container.lastChild);
+  // }
   for (i=0;i<number;i++){
       // Append a node with a random text
-      container.appendChild(document.createTextNode("Member " + (i+1)));
-      // Create an <input> element, set its type and name attributes
-      var input = document.createElement("input");
-      input.type = "text";
-      input.name = "member" + i;
-      container.appendChild(input);
-      // Append a line break 
-      container.appendChild(document.createElement("br"));
-  }
+      // container.appendChild(document.createTextNode("Member " + (i+1)));
+      // // Create an <input> element, set its type and name attributes
+      // var input = document.createElement("input");
+      // input.type = "text";
+      // input.name = "member" + i;
+      // container.appendChild(input);
+      // // Append a line break 
+      // container.appendChild(document.createElement("br"));
+      container.appendChild(document.createTextNode("Member"));
+    }
 }
 
 var clear_button = document.getElementById("reset_button");
