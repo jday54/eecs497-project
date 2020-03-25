8ab4ba3feb3f2121eac53fce9c101be9b8ad6616
Austin Wong <auswong@umich.edu>
2020-03-25 16:15:16 -0400

    small push

--- js/options.js
+++ js/options.js
@@ -23,24 +23,25 @@ chrome.storage.sync.get({notecards : {}}, function(items) {
   // create a new div element 
   var numMemorize = document.getElementById("numMemorize");
 
-  let num_of_notecards = 0;
+  let numOfNotecards = 0;
   for (let [key, value] of Object.entries(notecards)) {
-    num_of_notecards = num_of_notecards + 1;
+    numOfNotecards = numOfNotecards + 1;
   }
-  numMemorize.innerHTML = "You have " + num_of_notecards + " things to memorize";
+  numMemorize.innerHTML = "You have " + numOfNotecards + " things to memorize";
 
-  var list_head = document.createElement("ul"); 
+  var listHead = document.createElement("ul"); 
+  listHead.setAttribute('class', 'reviewList');
   // give it some content
   for (let notecard of Object.values(notecards)) {
     var newListElem = document.createElement("li");
     newListElem.innerHTML = notecard.text;
     console.log(notecard.text);
     // add the text node to the newly created div
-    list_head.appendChild(newListElem);
+    listHead.appendChild(newListElem);
   };
   // add the newly created element and its content into the DOM 
   var resetButton = document.getElementById("resetButtonDiv"); 
-  resetButton.insertBefore(list_head, document.getElementById("resetButton"));   
+  resetButton.insertBefore(listHead, document.getElementById("resetButton"));   
 });
 
 https://stackoverflow.com/questions/14853779/adding-input-elements-dynamically-to-form
