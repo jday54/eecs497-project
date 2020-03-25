a8cf81a704217ca4c68ac91e3ffb7630a61949ab
johnmday <johnmday@umich.edu>
2020-03-25 15:54:13 -0400

    changed notecards data structure: is now an object with keys of date

--- js/background.js
+++ js/background.js
@@ -8,24 +8,25 @@ chrome.runtime.onInstalled.addListener(function() {
         contexts: ["selection"]
     });
     chrome.storage.sync.set({"threshold": 1000*5}); // 5 seconds
-    chrome.storage.sync.set({"notecards": []});
+    chrome.storage.sync.set({"notecards": {}});
     // var decks = Set()
 });
 
 // Store highlighted text  
 function onClickHandler(info) {
     if (info.menuItemId == "memorizer") {
-        chrome.storage.sync.get({notecards : []}, function(result){
+        chrome.storage.sync.get({notecards : new Map()}, function(result){
             let notecards = result.notecards;
-            let notecard = {"text" : info.selectionText,"date" : new Date().getTime(),"times_reminded":0};
-            notecards.push(notecard);
+            let date = new Date().getTime();
+            let notecard = {"text" : info.selectionText, "date" : date, "times_reminded":0};
+            notecards[date] = notecard;
             chrome.storage.sync.set({'notecards':notecards}, function() {
                 alert("You have selected -- " + notecard.text + " -- to memorize!");
                 console.log("Storage sync called with notecard:", notecard);
                 // ---For Debugging---
                 console.log("Current state of notecards (in no particular order):");
-                for (var i = 0; i < notecards.length; i++) {
-                    console.log("Notecard "+i+":", notecards[i]);
+                for (let [key, value] of Object.entries(notecards)) {
+                    console.log(key, "data: ", value);
                 }
                 // ---For Debugging---
                 });
@@ -36,17 +37,25 @@ function onClickHandler(info) {
 // Run onClickHandler when context menu item is selected
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
+function updateTimesReminded(info) {
+    chrome.storage.sync.get({notecards : []}, function(result){
+        let notecards = result.notecards;
+        console.log("ERROR: NOT IMPLEMENTED");
+    });
+};
+
 // Check for terms to send reminder for upon new tab creation
 chrome.tabs.onCreated.addListener(function() {
     chrome.storage.sync.get(['notecards','threshold'], function(result) {
         console.log("Checking for terms to memorize.")
         var threshold = result.threshold
         let notecards = result.notecards
-        for (let notecard of notecards) {
+        for (let [date, notecard] of Object.entries(notecards)) {
             // alert("text: "+ text + " | date: " + date);
             let current_date = new Date().getTime();
             if ((current_date - notecard.date) > threshold) {
-                // TODO: update times_reminded for notecard 
+                // TODO: update times_reminded for notecard
+                updateTimesReminded();
                 alert("Reminder to memorize: " + notecard.text);
                 console.log("Reminder sent for: " + notecard.text);
             }
