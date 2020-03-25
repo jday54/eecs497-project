20121078f0f7b3fbec4be4156ee1c5d8b2b66d92
Austin Wong <auswong@umich.edu>
2020-03-25 16:56:42 -0400

    small push

--- js/options.js
+++ js/options.js
@@ -51,13 +51,7 @@ chrome.storage.sync.get({notecards : {}}, function(items) {
 
 var resetButton = document.getElementById("resetButton");
 resetButton.addEventListener('click', function() {
-  // chrome.storage.sync.clear(function() {
-  //   console.log("all items cleared");
-  // })
-  
   chrome.storage.sync.set({"notecards": {}});
-  alert(chrome.storage.sync.get('threshold'));
-
   window.location.reload();
 });
 
